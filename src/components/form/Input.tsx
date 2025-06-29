7a0731028b0dbec33ea4cf77073a59c301c7a2c2
ammrbhlwn <muammar.bahalwan@gmail.com>
2025-06-30 01:07:18 +0700

    chore: change icon password

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -150,7 +150,7 @@ export default function Input({
               )}
               onClick={() => setShowPassword(!showPassword)}
             >
-              {showPassword ? <Eye /> : <EyeOff />}
+              {showPassword ? <EyeOff /> : <Eye />}
             </div>
           )}
         </div>
